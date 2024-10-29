acc8135ef17f2f2aa169b9dcf701266c06667594
developer-mixa <mixail_081177@mail.ru>
2024-10-29 10:25:47 +0300

    refactoring: validators are not responsible for messages

--- src/apps/bot/validators/validators.py
+++ src/apps/bot/validators/validators.py
@@ -1,37 +1,30 @@
-import re
-
-from .errors import ValidationError
+from . import errors as e
 from .base.base_validator import BaseTgValidator
 
 MAX_NAME_LEN = 15
 MIN_NAME_LEN = 2
 
+MIN_AGE = 0
+MAX_AGE = 200
+
 class NameValidator(BaseTgValidator):
     def _do_validate(self, message: str):
         if not message.isalpha():
-            raise ValidationError('Имя может содержать только буквы')
+            raise e.NameCanContainLettersError
         if len(message) >= MAX_NAME_LEN:
-            raise ValidationError('Не бывает такого длинного имени')
+            raise e.TooLongNameError
         if len(message) < MIN_NAME_LEN:
-            raise ValidationError('Вас назвали буквой? Поправьте имя')
+            raise e.TooShortNameError
         if message.count(' ') > 0 or message.count('\n') > 0:
-            raise ValidationError('Имя не может содержать пробелы, уберите их')
+            raise e.NameCannotContainSpacesError
         if message[0].islower():
-            raise ValidationError(f'Уважайте себя, напишите своё имя с большой буквы - {message.title()}')
-
+            raise e.NameBeginCannotBeLowercaseError
 
 class AgeValidator(BaseTgValidator):
     def _do_validate(self, message: str):
         if not message.isdigit():
-            raise ValidationError('Возраст должен быть числом')
-        if int(message) < 0:
-            raise ValidationError('Вы не можете быть настолько молодыми, число должно быть положительным')
-        if int(message) >= 200:
-            raise ValidationError('Леее, врятли вам 200+ лет')
-
-
-class GenderValidator(BaseTgValidator):
-
-    def _do_validate(self, message: str):
-        if message != 'Парень' and message != 'Девушка':
-            raise ValidationError('Леее, пол должен быть или "Парень", или "Девушка", третьего не дано')
+            raise e.AgeMustBeIntegerError
+        if int(message) < MIN_AGE:
+            raise e.AgeLessThanZeroError
+        if int(message) >= MAX_AGE:
+            raise e.TooBigAgeError
