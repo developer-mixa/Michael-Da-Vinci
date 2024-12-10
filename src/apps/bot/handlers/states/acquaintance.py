7d9c37f8c5decf6b0f1de693e756e45fdc8b6ee9
developer-mixa <mixail_081177@mail.ru>
2024-12-10 14:43:14 +0300

    feat: Added base acquaintance bot handling

---
+++ src/apps/bot/handlers/states/acquaintance.py
@@ -0,0 +1,4 @@
+from aiogram.fsm.state import StatesGroup, State
+
+class Acquaintance(StatesGroup):
+    finding = State()
---
+++ src/apps/bot/keyboards/acquaintance.py
@@ -0,0 +1,6 @@
+from aiogram.types import KeyboardButton
+
+from src.apps.bot.emoji.emojies import LIKE, DISLIKE, STOP
+from .utils import create_single_row_buttons
+
+CHOISES = create_single_row_buttons([KeyboardButton(text=LIKE), KeyboardButton(text=DISLIKE), KeyboardButton(text=STOP)])
