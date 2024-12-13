38b9dc75d0fb3c8c847569e9ce18697b42cf479b
developer-mixa <mixail_081177@mail.ru>
2024-12-13 12:26:40 +0300

    refacroting: Improved bot responses, removals to constants

--- src/apps/bot/handlers/acquaintance/handler.py
+++ src/apps/bot/handlers/acquaintance/handler.py
@@ -1,24 +1,33 @@
-import base64
 import logging
-from aiogram.fsm.context import FSMContext
-from aiogram.types import Message
+
 from aiogram import F
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message, ReplyKeyboardRemove
+from aiogram.types.input_file import BufferedInputFile
+
 from config.settings import settings
 from src.apps.bot.bot import get_bot
-from src.apps.bot.emoji.emojies import STOP, LIKE, DISLIKE
+from src.apps.bot.commands.commands import FIND_LOVE
+from src.apps.bot.emoji.emojies import DISLIKE, LIKE, STOP
 from src.apps.bot.handlers.states.acquaintance import Acquaintance
 from src.apps.bot.keyboards.acquaintance import CHOISES
-from src.apps.bot.producers.acquaintance_producer import AcquaintanceProducer
-from src.apps.consumers.acquaintance_consumer.schema.acquintance_data import MutualityData, SearchAcquaintanceData, LikeUserData
-from src.apps.consumers.acquaintance_consumer.schema.responses.__main__ import AcquaintanceResponse, AcquaintanceResponseStatus, LikedResponseStatus
-from src.apps.consumers.common.user_data import UserData
-from src.apps.consumers.register_consumer.schema.registration import RegistrationData
-from .router import router
-from src.apps.bot.commands.commands import FIND_LOVE
-from aiogram.types import ReplyKeyboardRemove
 from src.apps.bot.messages import acquaintance as msg
-from aiogram.types.input_file import InputFile, BufferedInputFile
+from src.apps.bot.producers.acquaintance_producer import AcquaintanceProducer
+from src.apps.consumers.acquaintance_consumer.schema.acquintance_data import (
+    LikeUserData,
+    MutualityData,
+    SearchAcquaintanceData,
+)
+from src.apps.consumers.acquaintance_consumer.schema.responses.responses import (
+    AcquaintanceResponse,
+    AcquaintanceResponseStatus,
+    LikedResponseStatus,
+)
+from src.apps.consumers.common.user_data import UserData
 
+from src.apps.consumers.acquaintance_consumer.actions import LIKE as LIKE_ACTION, SEARCH as SEARCH_ACTION
+
+from .router import router
 
 acquaintance_producer = AcquaintanceProducer()
 
@@ -26,13 +35,9 @@ logger = logging.getLogger(__name__)
 
 @router.message(F.text == FIND_LOVE)
 async def start_find(message: Message, state: FSMContext):
-    # finding love...
-
     await state.clear()
     await state.set_state(Acquaintance.finding)
     await send_acquaintance_answer(message, state)
-
-# Через state сделать
 
 @router.message(Acquaintance.finding)
 async def finding(message: Message, state: FSMContext):
@@ -45,15 +50,14 @@ async def finding(message: Message, state: FSMContext):
     elif message_text == LIKE:
         liked_user_data = await state.get_data()
         liked_user_id = liked_user_data['current_user_id']
-        liked_data = LikeUserData(user_id=message.from_user.id, liked_user_id=liked_user_id, action='like_user')
+        liked_data = LikeUserData(user_id=message.from_user.id, liked_user_id=liked_user_id, action=LIKE_ACTION)
         async with acquaintance_producer as producer:
             await producer.base_produce_message(liked_data, settings.ACQUAINTANCE_QUEUE_NAME)
             await producer.wait_answer_for_user(
                 settings.ACQUAINTANCE_LIKE_QUEUE_NAME,
                 message.from_user.id,
-                lambda response: __push_liked_answer(response, message)
+                lambda response: __push_liked_answer(response, message, state)
             )
-        await send_acquaintance_answer(message, state)
     elif message_text == DISLIKE:
         await send_acquaintance_answer(message, state)
     else:
@@ -61,7 +65,7 @@ async def finding(message: Message, state: FSMContext):
 
 async def send_acquaintance_answer(message: Message, state: FSMContext):
     user_id = message.from_user.id
-    search_data = SearchAcquaintanceData(user_id=user_id, action='search')
+    search_data = SearchAcquaintanceData(user_id=user_id, action=SEARCH_ACTION)
     async with acquaintance_producer as producer:
         await producer.base_produce_message(search_data, settings.ACQUAINTANCE_QUEUE_NAME)
         await producer.wait_answer_for_user(
@@ -75,31 +79,32 @@ async def __push_search_answer(response: AcquaintanceResponse, message: Message,
     if search_response == AcquaintanceResponseStatus.FOUND:
         found_user_data: UserData = response['data']
         image_input_file = BufferedInputFile(found_user_data['image'], found_user_data['name'])
-        await message.answer_photo(photo=image_input_file, caption=f'''
-        {found_user_data["name"]}, {found_user_data["age"]}
-        
-        {found_user_data['description']}
-        ''', reply_markup=CHOISES)
+        await message.answer_photo(photo=image_input_file, caption=msg.USER_INFO_TEMPLATE.format(
+            name=found_user_data['name'],
+            age=found_user_data['age'],
+            description=found_user_data['description']
+        ), reply_markup=CHOISES)
         await state.set_data({'current_user_id': found_user_data['user_id']})
     elif search_response == AcquaintanceResponseStatus.NON_REGISTERED:
-        await message.answer('Вы не зарегестрированы!')
+        await message.answer(msg.NOT_REGISTERED)
     elif search_response == AcquaintanceResponseStatus.NOT_FOUND:
-        await message.answer('К сожалению, не найдено ни одного пользователя =(')
+        await message.answer(msg.NO_USERS_FOUND)
     elif search_response == AcquaintanceResponseStatus.UNEXCEPTED_ERROR:
-        await message.answer('Что-то пошло не так... Попробуйте ещё раз или обратитесь к разработчику')
+        await message.answer(msg.SOMETHING_WENT_WRONG)
 
-async def __push_liked_answer(response: AcquaintanceResponse, message: Message):
+async def __push_liked_answer(response: AcquaintanceResponse, message: Message, state: FSMContext):
     liked_response = LikedResponseStatus.deserialize(response['response'])
     if liked_response == LikedResponseStatus.MUTUALLY:
         bot = get_bot()
         mutually_data: MutualityData = response['data']
         liked_user_chat = await bot.get_chat(mutually_data['liked_user_id'])
         liked_username = liked_user_chat.username
-        liked_text = 'Вы взаимно лайкнули друг друга, теперь вы можете продолжить в чате: @{username}'
-        await message.answer(liked_text.format(username=liked_username))
-        await bot.send_message(mutually_data['liked_user_id'], liked_text.format(username=message.from_user.username))
+        await message.answer(msg.MUTUALL_LIKE_TEMPLATE.format(username=liked_username), reply_markup=ReplyKeyboardRemove())
+        await bot.send_message(mutually_data['liked_user_id'], msg.MUTUALL_LIKE_TEMPLATE.format(username=message.from_user.username))
+        await state.clear()
     elif liked_response == LikedResponseStatus.LIKE_SENT:
         logger.info('Like successfully delivered from %s', message.from_user.id)
+        await send_acquaintance_answer(message, state)
     else:
         logger.info('Something went wrong... %s', liked_response)
-        await message.answer('Что-то пошло не так... Попробуйте ещё раз или обратитесь к разработчику')
+        await message.answer(msg.SOMETHING_WENT_WRONG)
