11120d750143217b26a14dc1cc152f0d4ddd60c6
developer-mixa <mixail_081177@mail.ru>
2024-12-16 15:01:10 +0300

    feat: Analytics for the number of messages sent in the bot

---
+++ src/apps/bot/middlewares/calculation_analytics.py
@@ -0,0 +1,13 @@
+from typing import Callable, Dict, Any, Awaitable
+from aiogram import BaseMiddleware
+from aiogram.types import TelegramObject
+from src.apps.common.analytics.metrics import TOTAL_BOT_SEND_MESSAGES 
+
+class CalculationAnalyticsMiddleware(BaseMiddleware):
+    async def __call__(self,
+                       handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+                       event: TelegramObject,
+                       data: Dict[str, Any]):
+        result = await handler(event, data)
+        TOTAL_BOT_SEND_MESSAGES.inc()
+        return result
