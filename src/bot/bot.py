020b3eded17e9de2475ae61766b973a7b9bdf6b0
developer-mixa <mixail_081177@mail.ru>
2025-04-04 17:17:16 +0300

    feat: setup webhook and redis

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,28 +1,23 @@
-import asyncio
-
-import logging
 from aiogram import Bot, Dispatcher
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
-from aiogram.fsm.storage.memory import MemoryStorage
-from src.bot.handlers.message.router import router
 
-from dotenv import load_dotenv
-from os import getenv
+bot: Bot
+dp: Dispatcher
 
-load_dotenv()
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
+def setup_bot(bot_: Bot) -> None:
+    global bot
+    bot = bot_
 
-bot = Bot(token=getenv('BOT_TOKEN'), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-dp = Dispatcher(storage=MemoryStorage())
+def get_bot() -> Bot:
+    global bot
 
-async def main():
-    dp.message("asdasdasd")
-    dp.include_router(router)
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+    return bot
 
-if __name__ == "__main__":
-    asyncio.run(main())
+def setup_dp(dp_: Dispatcher) -> None:
+    global dp
+    dp = dp_
+
+def get_dp() -> Dispatcher:
+    global dp
+
+    return dp
