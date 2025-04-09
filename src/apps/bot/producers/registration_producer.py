e8781cd7a5fa5808f2b1322bdadce76db3b1565a
developer-mixa <mixail_081177@mail.ru>
2025-04-09 15:15:00 +0300

    feat: Consumer communicates with bot about successful registration

--- src/apps/bot/producers/registration_producer.py
+++ src/apps/bot/producers/registration_producer.py
@@ -1,4 +1,8 @@
+import asyncio
+from typing import Callable, Coroutine
+
 from aio_pika import Message
+from aio_pika.exceptions import QueueEmpty
 
 from src.apps.consumers.register_consumer.register_updates_rabbit import RegisterUpdatesRabbit
 
@@ -15,6 +19,8 @@ logger = logging.getLogger(__name__)
 
 class RegistrationProducer(RegisterUpdatesRabbit):
 
+    __RETRIES = 5
+
     async def produce_message(self, registration_data: RegistrationData):
         logger.info("Producing message %s", registration_data)
 
@@ -27,3 +33,22 @@ class RegistrationProducer(RegisterUpdatesRabbit):
 
         await exchange.publish(message, settings.REGISTRATION_QUEUE_NAME)
         logger.warning("Produced message %s", message.body)
+
+    async def wait_register_answer(self, user_id, register_callback: Callable[[bool], Coroutine]):
+        channel = await self.channel()
+        await channel.set_qos(prefetch_count=1)
+        queue_name = f'{settings.REGISTRATION_QUEUE_NAME}.{user_id}'
+
+        logger.info("Registration handler started waiting for answer...")
+
+        queue = await self.declare_register_updates_queue(queue_name=queue_name, exclusive=not queue_name)
+        for _ in range(self.__RETRIES):
+            try:
+                logger.info("Try to get value from queue...")
+                is_reg = await queue.get()
+                parsed_is_reg: bool = msgpack.unpackb(is_reg.body)
+                logger.info("Got value from queue: %s", parsed_is_reg)
+                await register_callback(parsed_is_reg)
+                break
+            except QueueEmpty:
+                await asyncio.sleep(1)
