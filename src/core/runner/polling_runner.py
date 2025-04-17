1aa0068dd42e43622c3b57de81c0eec4bc9a02f5
developer-mixa <mixail_081177@mail.ru>
2025-04-17 18:45:00 +0300

    feat: Setup webhook

--- src/core/runner/polling_runner.py
+++ src/core/runner/polling_runner.py
@@ -1,28 +1,10 @@
 import asyncio
-
-from aiogram.fsm.storage.memory import MemoryStorage
-
-from src.apps.bot.bot import setup_dp, setup_bot
 from src.core.runner.base.bot_runner import BotRunner
 import logging
-from aiogram import Bot, Dispatcher
-from config.settings import settings
-from src.apps.bot.handlers.registration.router import router as registration_router
-from src.apps.bot.handlers.user_state.router import router as user_state_router
-from src.apps.bot.handlers.acquaintance.router import router as acquaintance_router
-from src.apps.bot.handlers.flood.router import router as flood_router
-from src.apps.bot.menu.commands_menu import bot_commands
 
 logger = logging.getLogger(__name__)
 
 class PollingRunner(BotRunner):
-
-    def __init__(self):
-        self.__dp = Dispatcher(storage=MemoryStorage())
-        setup_dp(self.__dp)
-
-        self.__bot = Bot(token=settings.BOT_TOKEN)
-        setup_bot(self.__bot)
 
     def run(self):
         asyncio.run(self.__run())
@@ -30,13 +12,8 @@ class PollingRunner(BotRunner):
     async def __run(self):
         logger.info('Starting polling')
 
-        await self.__bot.set_my_commands(bot_commands)
-
-        self.__dp.include_router(registration_router)
-        self.__dp.include_router(user_state_router)
-        self.__dp.include_router(acquaintance_router)
-        self.__dp.include_router(flood_router)
-        await self.__bot.delete_webhook()
+        await self._setup()
+        await self._bot.delete_webhook()
 
         logging.error('Dependencies launched')
-        await self.__dp.start_polling(self.__bot)
+        await self._dp.start_polling(self._bot)
--- src/core/runner/webhook_runner.py
+++ src/core/runner/webhook_runner.py
@@ -2,32 +2,25 @@ import uvicorn
 import logging
 
 from fastapi import FastAPI
-from aiogram import Bot, Dispatcher
-from src.apps.bot.bot import setup_dp, setup_bot
 from config.settings import settings
 from starlette_context.middleware import RawContextMiddleware
 from starlette_context import plugins
 from src.core.runner.base.bot_runner import BotRunner
 
+import asyncio
+
 logger = logging.getLogger(__name__)
 
 class WebhookRunner(BotRunner):
     def run(self):
+        asyncio.run(self.__run())
+
+    async def __run(self):
+        await self._setup()
+        await self._bot.set_webhook(settings.BOT_WEBHOOK_URL)
         uvicorn.run('src.core.runner.webhook_runner:create_app', factory=True, host='0.0.0.0', port=8000, workers=1)
 
-
-async def lifespan(app: FastAPI) -> None:
-
-    logger.info('Starting lifespan')
-
-    dp = Dispatcher()
-    setup_dp(dp)
-    bot = Bot(token=settings.BOT_TOKEN)
-    setup_bot(bot)
-
-    await bot.set_webhook(settings.BOT_WEBHOOK_URL)
-
 def create_app() -> FastAPI:
-    app = FastAPI(docs_url='/swagger', lifespan=lifespan)
+    app = FastAPI(docs_url='/swagger')
     app.add_middleware(RawContextMiddleware, plugins=[plugins.CorrelationIdPlugin()])
     return app
